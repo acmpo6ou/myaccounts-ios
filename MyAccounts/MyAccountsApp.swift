6e01b04cd31fe5a7f1e02ef5e7c604498992bc56
acmpo6ou <acmpo6ou@gmail.com>
2023-06-28 06:54:02 +0200

    Initial Commit

---
+++ MyAccounts/MyAccountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAccountsApp.swift
+//  MyAccounts
+//
+//  Created by Catalyst on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAccountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
