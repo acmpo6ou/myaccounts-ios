d570fdf046a52480cb2aca3cc327c1ed9a9ae424
acmpo6ou <acmpo6ou@gmail.com>
2023-08-05 11:00:45 +0200

    ⚙️  Implement Settings UI

---
+++ MyAccounts/Other/Settings.swift
@@ -0,0 +1,43 @@
+// Copyright (C) 2023. Bohdan Kolvakh
+// This file is part of MyAccounts.
+// 
+// MyAccounts is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import SwiftUI
+
+struct Settings: View {
+    @StateObject var viewModel = SettingsViewModel()
+    var body: some View {
+        Form {
+            VStack {
+                Toggle("BlockScreen".l, isOn: $viewModel.blockScreen)
+                Text("BlockScreenTip".l)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            VStack {
+                Toggle("LockApp".l, isOn: $viewModel.lockApp)
+                Text("LockAppTip".l)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
